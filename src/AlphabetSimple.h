fa47fad4ce9c0db2267ff28aed6f23f81e68c281
Laura <erdaradun.gaztea+github@gmail.com>
2025-01-05 21:15:52 +0100

    headerify the other 2 classes

--- src/AlphabetSimple.h
+++ src/AlphabetSimple.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <cmath>
 #include <string>
 #include <vector>
@@ -5,29 +6,36 @@
 
 #include "Alphabet.h"
 
-class AlphabetSimple : public Alphabet<char> {
+
+class AlphabetSimple final : public Alphabet<char> {
 public:
-  AlphabetSimple(std::vector<char> letters) : Alphabet<char>(letters) {}
+  explicit AlphabetSimple(const std::vector<char> &letters);
 
-  std::vector<int> pack(const std::string& string) {
-    // TODO: pack instead of simply indexing
-    std::vector<int> ret;
-    int max_index = pow(2, width_) - 1;
-    std::transform(string.cbegin(), string.cend(), std::back_inserter(ret), [this, max_index](char s) {
-      auto result = std::find(letters_.cbegin(), letters_.cend(), s);
-      return result != letters_.cend() ? std::distance(letters_.cbegin(), result) : max_index;
-    });
-    return ret;
-  }
-
-  std::string unpack(const std::vector<int>& packed) {
-    std::vector<char> unpacked;
-    int max_index = pow(2, width_) - 1;
-    std::transform(packed.cbegin(), packed.cend(), std::back_inserter(unpacked), [this, max_index](int index) {
-      // TODO: do not hardcode missing letter
-      return index == max_index ? '!' : letters_.at(index);
-    });
-    std::string ret(unpacked.cbegin(), unpacked.cend());
-    return ret;
-  }
+  std::vector<int> pack(const std::string& string) override;
+  std::string unpack(const std::vector<int>& packed) override;
 };
+
+
+inline AlphabetSimple::AlphabetSimple(const std::vector<char> &letters) : Alphabet<char>(letters) {};
+
+inline std::vector<int> AlphabetSimple::pack(const std::string& string) {
+  // TODO: pack instead of simply indexing
+  std::vector<int> ret;
+  int max_index = static_cast<int>(pow(2, width_)) - 1;
+  std::transform(string.cbegin(), string.cend(), std::back_inserter(ret), [this, max_index](const char s) {
+    const auto result = std::find(letters_.cbegin(), letters_.cend(), s);
+    return result != letters_.cend() ? std::distance(letters_.cbegin(), result) : max_index;
+  });
+  return ret;
+}
+
+inline std::string AlphabetSimple::unpack(const std::vector<int>& packed) {
+  std::vector<char> unpacked;
+  int max_index = static_cast<int>(pow(2, width_)) - 1;
+  std::transform(packed.cbegin(), packed.cend(), std::back_inserter(unpacked), [this, max_index](const int index) {
+    // TODO: do not hardcode missing letter
+    return index == max_index ? '!' : letters_.at(index);
+  });
+  std::string ret(unpacked.cbegin(), unpacked.cend());
+  return ret;
+}
--- src/TinyStrings.h
+++ src/TinyStrings.h
@@ -1,68 +1,84 @@
+#pragma once
 #include <cmath>
+#include <utility>
 #include <vector>
 #include <algorithm>
 
 #include "AlphabetSimple.h"
+
 
 class TinyStrings {
   std::vector<std::vector<int>> data_;
   AlphabetSimple alphabet_;
 
 public:
-  TinyStrings(std::vector<std::string> strings, AlphabetSimple alphabet) : alphabet_(alphabet) {
-    std::transform(strings.cbegin(), strings.cend(), std::back_inserter(data_), [this](std::string s) {
-      return alphabet_.pack(s);
-    });
-  }
+  TinyStrings(const std::vector<std::string>& strings, AlphabetSimple alphabet);
+  TinyStrings(const std::vector<std::string>& strings, const std::vector<char>& letters);
 
-  TinyStrings(std::vector<std::string> strings, std::vector<char> letters) : TinyStrings(strings, AlphabetSimple(letters)) {}
+  std::vector<std::string> unpack();
+  [[nodiscard]] TinyStrings* flattened() const;
+  [[nodiscard]] std::vector<std::vector<int>::size_type> sizes() const;
+
+  std::vector<std::vector<int>>& get_data();
+  AlphabetSimple& get_alphabet();
 
 private:
-  TinyStrings(std::vector<std::vector<int>> data, AlphabetSimple alphabet) : data_(data), alphabet_(alphabet) {}
-
-  TinyStrings(std::vector<int> scalar_data, AlphabetSimple alphabet) : alphabet_(alphabet) {
-    data_.push_back(scalar_data);
-  }
-
-public:
-  std::vector<std::string> unpack() {
-    std::vector<std::string> ret;
-    std::transform(data_.cbegin(), data_.cend(), std::back_inserter(ret), [this](std::vector<int> s) {
-      return alphabet_.unpack(s);
-    });
-    return ret;
-  }
-
-  TinyStrings* flattened() {
-    std::size_t total_size = 0;
-    for (auto s : data_) {
-      total_size += s.size();
-    }
-
-    std::vector<int> flat_data;
-    flat_data.reserve(total_size);
-
-    for (auto s : data_) {
-      flat_data.insert(flat_data.end(), s.cbegin(), s.cend());
-    }
-
-    auto flattened = new TinyStrings(flat_data, alphabet_);
-    return flattened;
-  }
-
-  std::vector<std::vector<int>::size_type> sizes() {
-    std::vector<std::vector<int>::size_type> ret;
-    std::transform(data_.cbegin(), data_.cend(), std::back_inserter(ret), [this](std::vector<int> s) {
-      return std::ceil(s.size() * alphabet_.get_width() / 8.0);
-    });
-    return ret;
-  }
-
-  std::vector<std::vector<int>>& get_data() {
-    return data_;
-  }
-
-  AlphabetSimple& get_alphabet() {
-    return alphabet_;
-  }
+  TinyStrings(const std::vector<std::vector<int>> &data, AlphabetSimple alphabet);
+  TinyStrings(const std::vector<int> &scalar_data, AlphabetSimple alphabet);
 };
+
+
+inline TinyStrings::TinyStrings(const std::vector<std::string>& strings, AlphabetSimple alphabet) : alphabet_(std::move(alphabet)) {
+  std::transform(strings.cbegin(), strings.cend(), std::back_inserter(data_), [this](const std::string& s) {
+    return alphabet_.pack(s);
+  });
+}
+
+inline TinyStrings::TinyStrings(const std::vector<std::string>& strings, const std::vector<char>& letters) : TinyStrings(strings, AlphabetSimple(letters)) {}
+
+inline std::vector<std::string> TinyStrings::unpack() {
+  std::vector<std::string> ret;
+  std::transform(data_.cbegin(), data_.cend(), std::back_inserter(ret), [this](const std::vector<int>& s) {
+    return alphabet_.unpack(s);
+  });
+  return ret;
+}
+
+inline TinyStrings* TinyStrings::flattened() const {
+  std::size_t total_size = 0;
+  for (const auto& s : data_) {
+    total_size += s.size();
+  }
+
+  std::vector<int> flat_data;
+  flat_data.reserve(total_size);
+
+  for (const auto& s : data_) {
+    flat_data.insert(flat_data.end(), s.cbegin(), s.cend());
+  }
+
+  const auto flattened = new TinyStrings(flat_data, alphabet_);
+  return flattened;
+}
+
+inline std::vector<std::vector<int>::size_type> TinyStrings::sizes() const {
+  std::vector<std::vector<int>::size_type> ret;
+  std::transform(data_.cbegin(), data_.cend(), std::back_inserter(ret), [this](const std::vector<int>& s) {
+    return std::ceil(static_cast<double>(s.size()) * alphabet_.get_width() / 8.0);
+  });
+  return ret;
+}
+
+inline std::vector<std::vector<int>>& TinyStrings::get_data() {
+  return data_;
+}
+
+inline AlphabetSimple& TinyStrings::get_alphabet() {
+  return alphabet_;
+}
+
+inline TinyStrings::TinyStrings(const std::vector<std::vector<int>> &data, AlphabetSimple alphabet) : data_(data), alphabet_(std::move(alphabet)) {}
+
+inline TinyStrings::TinyStrings(const std::vector<int> &scalar_data, AlphabetSimple alphabet) : alphabet_(std::move(alphabet)) {
+  data_.push_back(scalar_data);
+}
