89e69d44c7f3c1b8b87559f63a045e9b2b804845
Laura <erdaradun.gaztea+github@gmail.com>
2026-02-18 23:14:08 +0100

    optimize `rcpp_unpack()`

--- src/r_unpack.cpp
+++ src/r_unpack.cpp
@@ -11,10 +11,13 @@
 cpp11::writable::strings rcpp_unpack(SEXP x) {
     const cpp11::external_pointer<TinyStrings> x_ptr(x);
     assert_not_null_pointer(x_ptr);
+
     const auto unpacked = x_ptr->unpack();
-    cpp11::writable::strings ret;
-    for (const auto &sequence: unpacked) {
-        ret.push_back(sequence);
-    }
+
+    cpp11::writable::strings ret(
+        unpacked.cbegin(),
+        unpacked.cend()
+    );
+
     return ret;
 }
